b5939365cb30924841f1a5e6c1170eb70e061eac
gytjd <hys339631@gmail.com>
2024-10-08 16:37:33 +0900

    #12 Feat: S3Service 생성 및 사용자별 디렉토리 생성 기능 추가
    - main.tf 파일 추후 수정 필요

---
+++ src/main/java/AIWA/McpBackend/service/s3/S3Service.java
@@ -0,0 +1,46 @@
+package AIWA.McpBackend.service.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3 s3Client;
+    private final String bucketName = "your-s3-bucket-name";
+
+    public void createUserDirectory(String userId) {
+        String userPrefix = "users/" + userId + "/";
+
+        // S3에 빈 파일을 업로드해서 디렉토리 구조 생성
+        s3Client.putObject(bucketName, userPrefix, "");
+
+        // Terraform 파일 초기화
+        uploadInitialTerraformFiles(userPrefix);
+    }
+
+    private void uploadInitialTerraformFiles(String userPrefix) {
+        // main.tf 파일 초기화
+        String mainTfContent = """
+            provider "aws" {
+              region = "us-east-1"
+            }
+            resource "aws_instance" "example" {
+              ami           = "ami-0c55b159cbfafe1f0"
+              instance_type = "t2.micro"
+              tags = {
+                Name = "Terraform-EC2"
+              }
+            }
+        """;
+
+        s3Client.putObject(bucketName, userPrefix + "main.tf", mainTfContent);
+
+        // 빈 상태 파일 초기화
+        String emptyState = "{}";
+        s3Client.putObject(bucketName, userPrefix + "terraform.tfstate", emptyState);
+    }
+
+}
