3076e4c1fecd9c3813d21c2ef4b11417a9384add
gytjd <hys339631@gmail.com>
2024-10-21 09:58:59 +0900

    #55 Refac: RouteTableController Response 수정

--- src/main/java/AIWA/McpBackend/provider/aws/api/controller/routetable/RouteTableController.java
+++ src/main/java/AIWA/McpBackend/provider/aws/api/controller/routetable/RouteTableController.java
@@ -3,7 +3,9 @@ package AIWA.McpBackend.provider.aws.api.controller.routetable;
 import AIWA.McpBackend.provider.aws.api.dto.routetable.RouteAddRequestDto;
 import AIWA.McpBackend.provider.aws.api.dto.routetable.RouteTableRequestDto;
 import AIWA.McpBackend.provider.aws.api.dto.routetable.RouteTableSubnetAssociationRequestDto;
+import AIWA.McpBackend.provider.response.CommonResult;
 import AIWA.McpBackend.service.aws.routetable.RouteTableService;
+import AIWA.McpBackend.service.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,28 +16,30 @@ public class RouteTableController {
 
     private final RouteTableService routeTableService;
 
+    private final ResponseService responseService;
+
     /**
      * 라우트 테이블 생성
      */
     @PostMapping("/create")
-    public String createRouteTable(@RequestBody RouteTableRequestDto routeTableRequestDto) {
+    public CommonResult createRouteTable(@RequestBody RouteTableRequestDto routeTableRequestDto) {
         try {
             routeTableService.createRouteTable(
                     routeTableRequestDto.getRouteTableName(),
                     routeTableRequestDto.getVpcName(),
                     routeTableRequestDto.getUserId()
             );
-            return "Route Table 생성 요청이 성공적으로 처리되었습니다.";
+            return responseService.getSuccessResult();
         } catch (Exception e) {
             e.printStackTrace();
-            return "Route Table 생성 중 오류가 발생했습니다: " + e.getMessage();
+            return responseService.getFailResult();
         }
     }
     /**
      * 라우트 테이블에 게이트웨이 또는 엔드포인트로 라우트 추가
      */
     @PostMapping("/add-route")
-    public String addRoute(@RequestBody RouteAddRequestDto routeAddRequestDto) {
+    public CommonResult addRoute(@RequestBody RouteAddRequestDto routeAddRequestDto) {
         try {
             routeTableService.addRoute(
                     routeAddRequestDto.getRouteTableName(),
@@ -44,10 +48,10 @@ public class RouteTableController {
                     routeAddRequestDto.getGatewayId(),
                     routeAddRequestDto.getUserId()
             );
-            return "Route 추가 요청이 성공적으로 처리되었습니다.";
+            return responseService.getSuccessResult();
         } catch (Exception e) {
             e.printStackTrace();
-            return "Route 추가 중 오류가 발생했습니다: " + e.getMessage();
+            return responseService.getFailResult();
         }
     }
 
@@ -55,17 +59,17 @@ public class RouteTableController {
      * 라우트 테이블을 서브넷과 연결
      */
     @PostMapping("/associate-subnet")
-    public String associateRouteTableWithSubnet(@RequestBody RouteTableSubnetAssociationRequestDto requestDto) {
+    public CommonResult associateRouteTableWithSubnet(@RequestBody RouteTableSubnetAssociationRequestDto requestDto) {
         try {
             routeTableService.associateRouteTableWithSubnet(
                     requestDto.getRouteTableName(),
                     requestDto.getSubnetName(),
                     requestDto.getUserId()
             );
-            return "Route Table과 Subnet 연결이 성공적으로 처리되었습니다.";
+            return responseService.getSuccessResult();
         } catch (Exception e) {
             e.printStackTrace();
-            return "Route Table과 Subnet 연결 중 오류가 발생했습니다: " + e.getMessage();
+            return responseService.getFailResult();
         }
     }
 }
