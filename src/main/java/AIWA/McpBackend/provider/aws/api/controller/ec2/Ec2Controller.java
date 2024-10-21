60750e5df25262a01717dd6f5a3839e4f054ac44
gytjd <hys339631@gmail.com>
2024-10-21 10:21:51 +0900

    #55 Refac: Ec2Controller Response 수정

--- src/main/java/AIWA/McpBackend/provider/aws/api/controller/ec2/Ec2Controller.java
+++ src/main/java/AIWA/McpBackend/provider/aws/api/controller/ec2/Ec2Controller.java
@@ -1,7 +1,9 @@
 package AIWA.McpBackend.provider.aws.api.controller.ec2;
 
 import AIWA.McpBackend.provider.aws.api.dto.ec2.Ec2RequestDto;
+import AIWA.McpBackend.provider.response.CommonResult;
 import AIWA.McpBackend.service.aws.ec2.Ec2Service;
+import AIWA.McpBackend.service.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,25 +14,27 @@ public class Ec2Controller {
 
     private final Ec2Service ec2InstanceService;
 
+    private final ResponseService responseService;
+
     @PostMapping("/create")
-    public String createEC2Instance(@RequestBody Ec2RequestDto instanceRequest, @RequestParam String userId) {
+    public CommonResult createEC2Instance(@RequestBody Ec2RequestDto instanceRequest, @RequestParam String userId) {
         try {
             ec2InstanceService.createEC2Instance(instanceRequest, userId);
-            return "EC2 인스턴스 생성 요청이 성공적으로 처리되었습니다.";
+            return responseService.getSuccessResult();
         } catch (Exception e) {
             e.printStackTrace();
-            return "EC2 인스턴스 생성 중 오류가 발생했습니다: " + e.getMessage();
+            return responseService.getFailResult();
         }
     }
 
     @DeleteMapping("/delete")
-    public String deleteEC2Instance(@RequestParam String instanceName, @RequestParam String userId) {
+    public CommonResult deleteEC2Instance(@RequestParam String instanceName, @RequestParam String userId) {
         try {
             ec2InstanceService.deleteEC2Instance(instanceName, userId);
-            return "EC2 인스턴스 삭제 요청이 성공적으로 처리되었습니다.";
+            return responseService.getSuccessResult();
         } catch (Exception e) {
             e.printStackTrace();
-            return "EC2 인스턴스 삭제 중 오류가 발생했습니다: " + e.getMessage();
+            return responseService.getFailResult();
         }
     }
 }
