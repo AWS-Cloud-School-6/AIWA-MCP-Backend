2c9c201ccde75368ba1b4d4a57bda9fc45d5e450
Blue <hys339631@gmail.com>
2024-10-29 21:46:51 +0900

    Update TerraformService.java

--- src/main/java/AIWA/McpBackend/service/terraform/TerraformService.java
+++ src/main/java/AIWA/McpBackend/service/terraform/TerraformService.java
@@ -73,7 +73,7 @@ public class TerraformService {
 
     private void executeRemoteCommands(String userId, String commands) throws IOException {
         // terraform-svc의 외부 IP 및 포트 설정 (application.properties에서 설정된 값을 사용)
-        String url = "https://" + "terraform-svc" + "/terraform/api/terraform/execute";
+        String url = "http://" + "terraform-svc" + "/terraform/api/terraform/execute";
 
         // HTTP 요청 생성
         RestTemplate restTemplate = new RestTemplate();
