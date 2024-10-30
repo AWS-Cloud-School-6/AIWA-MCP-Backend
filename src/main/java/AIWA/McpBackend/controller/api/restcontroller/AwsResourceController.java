8257828cde59aa72f89038fb1d2c6c1eac8fad9a
nareunhang <xuwrgaf@gmail.com>
2024-10-30 18:03:35 +0900

    Fix: Route update

--- src/main/java/AIWA/McpBackend/controller/api/restcontroller/AwsResourceController.java
+++ src/main/java/AIWA/McpBackend/controller/api/restcontroller/AwsResourceController.java
@@ -6,6 +6,7 @@ import AIWA.McpBackend.controller.api.dto.vpc.VpcTotalResponseDto;
 import AIWA.McpBackend.service.aws.AwsResourceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/aws")
 public class AwsResourceController {
 
     private final AwsResourceService awsResourceService;
