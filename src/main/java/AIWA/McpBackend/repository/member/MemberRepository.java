1ab817ed3a3c8d6c0ade29985ab2c9d160239c25
gytjd <hys339631@gmail.com>
2024-10-30 14:44:43 +0900

    #87 Delete: MemberRepository 삭제

--- src/main/java/AIWA/McpBackend/repository/member/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package AIWA.McpBackend.repository.member;
-
-import AIWA.McpBackend.entity.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email);
-}
