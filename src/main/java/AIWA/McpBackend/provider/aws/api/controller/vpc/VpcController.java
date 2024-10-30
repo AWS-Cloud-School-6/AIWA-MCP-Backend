0ac693475e1fac0e30799d972a50dcd0f396b787
nareunhang <80584830+nareunhang@users.noreply.github.com>
2024-10-30 12:26:38 +0900

    Update VpcController.java

--- src/main/java/AIWA/McpBackend/provider/aws/api/controller/vpc/VpcController.java
+++ src/main/java/AIWA/McpBackend/provider/aws/api/controller/vpc/VpcController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/vpc")
+@RequestMapping("/spring/api/vpc")
 @RequiredArgsConstructor
 public class VpcController {
 
